12006b995c774c3d5af7b230cc008a22022d8031
Lucas Dantas <lucas.lucantas38@gmail.com>
2018-01-08 19:42:36 -0200

    Add files via upload

---
+++ js/animations.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(".name > img").fadeIn(1500);
+    $(".name > h1").fadeIn(2500);
+    $(".name > p").fadeIn(3000);
+
+    setTimeout(function(){
+        $(".icons").fadeIn(2000);
+    },3000)
+});
