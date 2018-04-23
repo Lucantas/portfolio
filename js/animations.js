60cf9e24b6e2e87bc09e979a5c28b7018d91bf45
Lucas Dantas <lucas.lucantas38@gmail.com>
2018-04-23 16:08:19 -0300

    Delete animations.js

--- js/animations.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $(".name > img").fadeIn(1500);
-    $(".name > h1").fadeIn(2500);
-    $(".name > p").fadeIn(3000);
-
-    setTimeout(function(){
-        $(".icons").fadeIn(2000);
-    },3000)
-});
